6f13225e7b702b6c4c99c8bfd393fb197465dbd4
EC2 Default User <ec2-user@ip-172-31-26-46.us-east-2.compute.internal>
2021-09-29 14:50:23 +0000

    bonus

--- index.js
+++ index.js
@@ -27,10 +27,19 @@ function countEveryOdd(numbers){
         }
     }
     
-}
+}git
 
 //countEveryOdd(10)
 //5 
 function countEvens(numbs){
-  
+    let count = 0
+    for(let i = 0; i < numbs.length ; i++){
+        if(numbs[i] % 2 === 0){
+            count+=1
+        
+        }
+        
+    }
+  return count
 }
+console.log(countEvens()[1,2,3,4,5,6,7,8,9,12])
