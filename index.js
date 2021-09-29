a6ebf05f3aa9d8773cf3d23be5f09c5173767051
EC2 Default User <ec2-user@ip-172-31-26-46.us-east-2.compute.internal>
2021-09-29 14:41:22 +0000

    all functions done

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 //1
 function countToTen(){
+    for(let i = 0; i <= 10; i++)
+    console.log(i)
   
 }
-
+ //countToTen()
 //2
-function countFromOne(){
+function countFromOne(num){
+ for(let i = 0; i <= num ; i++)
+    console.log(i)
+    
   
 }
-
+//countFromOne(10)
 //3
-function countEveryEven(){
-  
+function countEveryEven(number){
+  for(let i = 2 ; i <= number; i+=2)
+  console.log(i)
 }
-
+//countEveryEven(10)
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(numbers){
+    for(let i = 1; i <= numbers; i++){
+        if( i % 2 !== 0){
+            console.log(i);
+        }
+    }
+    
 }
 
+//countEveryOdd(10)
 //5 
-function countEvens(){
+function countEvens(numbs){
   
 }
-
